176042a7efb4e2d94d4a5b51ca2608c84291985c
rafaldziuryk <rafal.dziuryk@appvinio.com>
2023-04-18 07:18:46 +0200

    bump to newer version

--- lib/main.dart
+++ lib/main.dart
@@ -58,9 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            Text(S.of(context).incrementLabel),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
